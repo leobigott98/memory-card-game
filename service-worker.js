58b9291184279df4d41fd67f5c7bb1df1976f1a7
leobigott98 <leonardo.bigott@correo.unimet.edu.ve>
2025-05-30 10:24:01 -0400

    fixed service worker

--- service-worker.js
+++ service-worker.js
@@ -18,34 +18,37 @@ const ASSETS = [
   '/icons/apple-touch-icon.png',
   '/icons/android-chrome-192x192.png',
   '/icons/android-chrome-512x512.png',
-  '/tt-hazelnuts-font-family-1748587365-0\TTHazelnuts-Regular-Trial-BF6401510bf3e3e.otf',
+  '/tt-hazelnuts-font-family-1748587365-0/TTHazelnuts-Regular-Trial-BF6401510bf3e3e.otf',
 ];
 
 // Cache assets on install
-self.addEventListener('install', event => {
+self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
+    caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll(ASSETS);
     })
   );
 });
 
 // Serve cached content when offline
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then(response => {
+    caches.match(event.request).then(function(response) {
       return response || fetch(event.request);
     })
   );
 });
 
 // Optional: Clean up old caches
-self.addEventListener('activate', event => {
+self.addEventListener('activate', function(event) {
   event.waitUntil(
-    caches.keys().then(keys => {
+    caches.keys().then(function(keys) {
       return Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-            .map(key => caches.delete(key))
+        keys.filter(function(key) {
+          return key !== CACHE_NAME;
+        }).map(function(key) {
+          return caches.delete(key);
+        })
       );
     })
   );
